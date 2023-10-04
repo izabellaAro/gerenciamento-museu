2fcfaedc6f2e4fb47d6001cb93e3df0233063207
Izabella Aro <izabella.aro20@gmail.com>
2023-10-03 23:26:43 -0300

    adiciona gerador de ingresso

--- gerencMuseu.c
+++ gerencMuseu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <TIME.H>
 
 int main()
 {
@@ -10,35 +11,38 @@ int main()
 }
 
 int opcaoExposicao, decisao, tipoIngresso, pagamento;
+char nomeVisitante;
 
 void menu()
 {  
     printf("\n\nMuseu Ada Lovelace\n\n");
     
     printf("Obras em cartaz:\n");
-    printf("1. 100 da semana da arte moderna; \n2. 150 anos de Santos Dumont; \n3. Jogos olímpicos Paris 2024; \n4. História da tecnologia;\n0. Sair.\n\n");
+    printf("1. 100 anos da semana da arte moderna; \n2. 150 anos de Santos Dumont; \n3. Jogos olímpicos Paris 2024; \n4. História da tecnologia;\n0. Sair.\n\n");
 
     scanf("%d", &opcaoExposicao);
+
+    system("cls ||clear");
 
     switch(opcaoExposicao)
     {
         case 1:
-            printf("Resumo da Obra 1\n\n");
+            printf("\nResumo da Obra 1\n\n");
             opcaoDecisao();
         break;
         
          case 2:
-            printf("Resumo da Obra 2\n\n");
+            printf("\nResumo da Obra 2\n\n");
             opcaoDecisao();
         break;
 
          case 3:
-            printf("Resumo da Obra 3\n\n");
+            printf("\nResumo da Obra 3\n\n");
             opcaoDecisao();
         break;
 
          case 4:
-            printf("Resumo da Obra 4\n\n");
+            printf("\nResumo da Obra 4\n\n");
            opcaoDecisao();
         break;
 
@@ -67,7 +71,7 @@ void sair(){
 }
 
 void opcaoDecisao(){
-    printf("Para adquirir o ingresso, digite 1. \nPara voltar ao menu aperte qualquer tecla.");
+    printf("Para adquirir o ingresso, digite 1. \nPara voltar ao menu aperte qualquer tecla.\n\n");
     scanf("%d", &decisao);
 
     if(decisao == 1) { 
@@ -89,7 +93,18 @@ void compraIngresso(){
             printf("\nSiga com o pagamento da entrada Inteira no valor de R$15,00\n\n");
             formaPagamento();
         break;    
+
+        case 2:
+            printf("\nSiga com o pagamento da entrada Meia no valor de R$7,50\n\n");
+            formaPagamento();
+        break;
+
+        case 3:
+            printf("Siga com a emissão do seu ingresso e apresente o comprovante de isenção na entrada da exposição escolhida :)");
+        break; 
     }
+
+    gerarIngresso();    
 }
 
 void formaPagamento(){
@@ -103,4 +118,12 @@ void formaPagamento(){
         printf("\nSiga com o pagamento via cartão de débito pela máquina de cartões!\n");
     }
     printf("\nPagamento realizado com sucesso :)");
+    sleep(51);
+}
+
+void gerarIngresso(){
+    system("cls || clear");
+    printf("Por gentileza, informe seu nome e sobrenome: ");
+    scanf("%s", &nomeVisitante);
+    printf("\n\nQue a sua visita seja incrível!\n\n");
 }
